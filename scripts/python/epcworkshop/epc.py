ba1c581ff434cb810f3c9c980e643cb0556dff5e
rubentack <131241921+rubentack@users.noreply.github.com>
2023-04-19 12:03:10 +0200

    final with bugs
    needs openai prob?

--- scripts/python/epcworkshop/epc.py
+++ scripts/python/epcworkshop/epc.py
@@ -1,3 +1,19 @@
+import os
+import openai
 
-def test():
-	print("hello")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def run_chatgpt(prompt,wrapper):
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": f"{wrapper} {prompt}"}
+  ],
+  temperature=1.0,
+  n=1
+)
+return completion.choices[0].message.content
+
+
+print(completion)
